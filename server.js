72496072ad7bab41b982ba78c2fb3c8a2705da8a
Hazel Wu <tinazx056@gmail.com>
2022-05-14 13:16:51 +0800

    feat: 捕捉錯誤 Uncaught Excaption, unhandledRejection

--- server.js
+++ server.js
@@ -10,3 +10,19 @@ connectDB()
 app.listen(PORT, () => {
   console.log(`Server is runnging at http://locahost:${PORT}`)
 })
+
+// Uncaught Excaption
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message)
+  console.log('捕捉到錯誤 Uncaught Exception! Shutting down...')
+  process.exit(1)
+})
+
+// 未捕捉到的 Catch
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLER REJECTION! Shutting down...')
+  console.log(err)
+  server.close(() => {
+    process.exit(1)
+  })
+})
